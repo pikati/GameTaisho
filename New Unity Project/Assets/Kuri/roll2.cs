aca7fab0ba6baa62a471baf68d503fae9ff06e3d
derumin777 <rikubon.0724@gmail.com>
2020-03-04 18:00:30 +0900

    HAL コロナウイルスに敗北。残念。 Scriptを追加。

---
+++ New Unity Project/Assets/Kuri/roll2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll2 : MonoBehaviour {
+
+    public GameObject silyatinyan;
+    public float rotateSpeed = 50.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Vector3.up y方向への単位ベクトルなので、x,z平面に回転する。
+        transform.RotateAround(silyatinyan.transform.position, Vector3.right, rotateSpeed * Time.deltaTime);
+
+    }
+}
