6028899693d41dd79d3b88f0545fd96ae71a6c1f
pikatan <pikapisu@gmail.com>
2020-04-24 23:18:18 +0900

    exe削除

---
+++ New Unity Project/Assets/Pikatan/Script/Other/DayNight/DisplayDayNight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDayNight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Material day;
+    [SerializeField]
+    private Material night;
+    void Start()
+    {
+        RenderSettings.skybox = day;
+    }
+
+    public void ChangeSky(bool isDay)
+    {
+        RenderSettings.skybox = isDay ? day : night;
+    }
+}
