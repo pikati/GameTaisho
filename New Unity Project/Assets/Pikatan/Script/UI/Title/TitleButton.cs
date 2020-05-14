18c05a52214b895b04f23a986b888c7cdf53f934
pikatan <pikapisu@gmail.com>
2020-05-14 14:25:27 +0900

    ボタンアニメーション追加

--- New Unity Project/Assets/Pikatan/Script/UI/Title/TitleButton.cs
+++ New Unity Project/Assets/Pikatan/Script/UI/Title/TitleButton.cs
@@ -2,30 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class TitleButton : MonoBehaviour
 {
-    private Button[] buttons;
+    [SerializeField]
+    private GameObject firstSelect;
     // Start is called before the first frame update
     void Start()
     {
-        //buttons = new Button[3];
-        //buttons[0] = transform.GetChild(0).gameObject.GetComponent<Button>();
-        //buttons[1] = transform.GetChild(1).gameObject.GetComponent<Button>();
-        //buttons[2] = transform.GetChild(2).gameObject.GetComponent<Button>();
-        //ColorBlock color = new ColorBlock();
-        //color.normalColor = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-        //color.selectedColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-        //color.pressedColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        //foreach(Button b in buttons)
-        //{
-        //    b.colors = color;
-        //}
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(buttons[0].onClick);
+        EventSystem.current.SetSelectedGameObject(firstSelect);
     }
 }
