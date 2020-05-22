8162d46cb5a7831f692e20494156e82e387550cd
pikatan <pikapisu@gmail.com>
2020-05-22 15:00:44 +0900

    コミット

--- New Unity Project/Assets/Pikatan/Script/Test/VolumTest.cs
+++ New Unity Project/Assets/Pikatan/Script/Test/VolumTest.cs
@@ -6,7 +6,7 @@ public class VolumTest : MonoBehaviour
 {
     private float h = 0.0f;
     private const float RADIUS = 0.5f;
-    private const int MAX = (int)RADIUS * 2;
+    private const int MAX = 1;
     private const int MIN = 0;
     private const float N1 = Mathf.PI / 6;
 
@@ -15,12 +15,13 @@ public class VolumTest : MonoBehaviour
     void Update()
     {
         float r = 0.5f;
-        float v = N1 * h * (3 *Mathf.Pow(Mathf.Sqrt(h * (2 * r - h)), 2) + h * h);
+        volume = N1 * h * (3 *Mathf.Pow(Mathf.Sqrt(h * (2 * r - h)), 2) + h * h);
     }
 
     private void OnTriggerStay(Collider other)
     {
         h = transform.position.y - (other.transform.position.y - 0.5f);
+        
         if (h < MIN) h = MIN;
         else if (h > MAX) h = MAX;
     }
