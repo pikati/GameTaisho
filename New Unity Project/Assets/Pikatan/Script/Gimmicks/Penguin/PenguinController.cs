e95954a6e86c193a6edca40a9a08ba182986aa59
pikatan <pikapisu@gmail.com>
2020-05-26 11:39:30 +0900

    子ペンギン収集機能実装

---
+++ New Unity Project/Assets/Pikatan/Script/Gimmicks/Penguin/PenguinController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Principal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PenguinController : MonoBehaviour
+{
+    public int penguinMax { get; set; }
+    public int penguinCount { get; set; } = 0;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        penguinMax = GameObject.FindGameObjectsWithTag("BabyPenguin").Length;
+        if(penguinMax == 0)
+        {
+            GameObject.Find("Penguin").SetActive(false);
+        }
+        else
+        {
+            text = GameObject.Find("PenguinText").GetComponent<Text>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = penguinCount.ToString() + " / " + penguinMax.ToString();
+    }
+}
---
+++ New Unity Project/Assets/Pikatan/Script/Player/PenguinPosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinPosition : MonoBehaviour
+{
+    private const int MAX = 5;
+    private GameObject[] penguinPos = new GameObject[MAX];
+    private bool[] isPenguin = new bool[MAX];
+    private PenguinController pc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform t = transform.Find("PenguinsPoint");
+        for (int i = 0; i < MAX; i++)
+        {
+            penguinPos[i] = t.GetChild(i).gameObject;
+            isPenguin[i] = false;
+        }
+        pc = GameObject.Find("PenguinController").GetComponent<PenguinController>();
+    }
+
+    public GameObject GetPenguinPosition()
+    {
+        for(int i = 0; i < MAX; i++)
+        {
+            if(!isPenguin[i])
+            {
+                isPenguin[i] = true;
+                pc.penguinCount++;
+                return penguinPos[i];
+            }
+        }
+        return null;
+    }
+}
