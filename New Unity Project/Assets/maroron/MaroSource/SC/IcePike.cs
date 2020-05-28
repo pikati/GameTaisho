feda007096645d54da45ce2bd0f84e696a8cb413
derumin777 <rikubon.0724@gmail.com>
2020-05-28 14:05:56 +0900

    でちゅ

---
+++ New Unity Project/Assets/maroron/MaroSource/SC/IcePike.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcePike : MonoBehaviour
+{
+
+    float Clock;//時間計測
+    float TwoClock;//時間計測
+    public GameObject icexPrefab;//
+    //Rigidbody rb;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Clock = 0.0f;//pop1
+        TwoClock = 0.0f;//pop2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Clock);
+        //Debug.Log(TwoClock);
+        
+        //一つ目のPOP
+        Clock += Time.deltaTime;//時間計測
+        if (Clock>=5.0f)
+        {
+            GameObject ICEX = Instantiate(icexPrefab);
+            ICEX.transform.position = new Vector3(72f, 61, 0);
+            //経過時間を初期化して再度時間計測を始める
+            Clock = 0.0f;
+        }
+
+        //二つ目のPOP
+            TwoClock += Time.deltaTime;//時間計測
+            if (TwoClock >= 12.0f)
+            {
+                GameObject ICEX = Instantiate(icexPrefab);
+                ICEX.transform.position = new Vector3(66, 61, 0);
+                //経過時間を初期化して再度時間計測を始める
+                TwoClock = 0.0f;
+            }
+            
+    }
+}
---
+++ New Unity Project/Assets/maroron/MaroSource/SC/Warp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+
+    public Vector3 vec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject sirokuma = GameObject.FindGameObjectWithTag("warp");
+
+        if (other.tag == "warp")
+        {
+            Debug.Log("HIT");
+            transform.position = new Vector3(vec.x, vec.y, vec.z);
+        }
+    }
+
+}
