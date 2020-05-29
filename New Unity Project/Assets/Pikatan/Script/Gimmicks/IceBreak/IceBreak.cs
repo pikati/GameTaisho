5b521319407ce8e69a554675f5f1c13ab031e926
pikatan <pikapisu@gmail.com>
2020-05-29 22:55:33 +0900

    壊れる足場とか変更

--- New Unity Project/Assets/Pikatan/Script/Gimmicks/IceBreak/IceBreak.cs
+++ New Unity Project/Assets/Pikatan/Script/Gimmicks/IceBreak/IceBreak.cs
@@ -8,32 +8,59 @@ public class IceBreak : MonoBehaviour
     [SerializeField]
     private int loadCapacity;
     Rigidbody[] rb;
-    // Start is called before the first frame update
+    private bool isColPlayer = false;
+    [SerializeField]
+    private float taeru = 0;
+    private float countTime = 0;
+    private int penNum = 0;
     void Start()
     {
         rb = new Rigidbody[transform.childCount];
         rb = gameObject.GetComponentsInChildren<Rigidbody>();
     }
 
+    private void Update()
+    {
+        BreakIce();
+    }
+
     private void BreakIce()
     {
-        foreach(Rigidbody r in rb)
+        if (!isColPlayer) return;
+        float dTime = Time.deltaTime;
+        countTime += dTime + penNum * 0.1f * dTime;
+        if(countTime > taeru)
         {
-            r.isKinematic = false;
-            r.transform.SetParent(null);
-            Destroy(r.gameObject, 2.0f);
+            foreach (Rigidbody r in rb)
+            {
+                r.isKinematic = false;
+                r.transform.SetParent(null);
+                Destroy(r.gameObject, 2.0f);
+            }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            if(collision.gameObject.GetComponent<PlayerManager>().penguinNum > loadCapacity)
-            {
-                BreakIce();
-            }
+            penNum = collision.gameObject.GetComponent<PlayerManager>().penguinNum;
+            //if (penNum > loadCapacity)
+            //{
+            //    isColPlayer = true;
+            //}
+            isColPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isColPlayer = false;
+            countTime = 0;
         }
     }
 }
